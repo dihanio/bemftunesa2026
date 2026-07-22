0138ce607d9a315eb043af6467b7c77011d24fbb
dihanio <diha.23212@mhs.unesa.ac.id>
2026-07-22 11:55:00 +0700

    fix(ims): add output standalone for docker build and add set -e to deploy script

--- ims/next.config.ts
+++ ims/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone", // Uncomment for Docker deployment
+  output: "standalone",
   images: {
     remotePatterns: [
       {
